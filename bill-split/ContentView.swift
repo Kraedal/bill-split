3620ec0430a2340584047073c4a60f1103d0ef13
kraedal <jacobong25@icloud.com>
2026-02-25 17:02:40 +0800

    Initial Commit

---
+++ bill-split/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  bill-split
+//
+//  Created by Jacob Ong on 2/25/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ bill-split/bill_splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  bill_splitApp.swift
+//  bill-split
+//
+//  Created by Jacob Ong on 2/25/26.
+//
+
+import SwiftUI
+
+@main
+struct bill_splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
